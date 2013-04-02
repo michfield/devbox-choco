e3e5b2077cb658692307aa39605ce599f527df4f
Michael Field <michfield@gmail.com>
2013-04-02 14:15:47 +0200

    Package Devbox-Common. Initial push

---
+++ Devbox-Common/tools/chocolateyInstall.ps1
@@ -0,0 +1,39 @@
+﻿# Exact package name. Directory will be created
+# in choco\lib based on this value
+#
+$pkgName = 'Devbox-Common'
+
+try
+{
+  $varName = 'chocolatey_bin_root'
+
+  # Helper function that updates current environment based on settings
+  # in registry, thus avoiding a need for session restart
+  #
+  Update-SessionEnvironment
+
+  # Set variable only if empty
+  #
+  $varValue = Get-Item Env:$varName -ErrorAction SilentlyContinue | Select -ExpandProperty Value -First 1
+
+  if ($varValue)
+  {
+    Write-Host "Doing nothing. Environment variable is already set: $varName = $varValue"
+  }
+  else {
+
+    $varValue = Join-Path $Env:SystemDrive '\Tools'
+
+    [Environment]::SetEnvironmentVariable($varName, $varValue, 'User')
+    Set-Content Env:\$varName $varValue
+
+    Write-Host "Configured environment variable: $varName = $varValue"
+  }
+
+  Write-ChocolateySuccess "$pkgName"
+}
+catch
+{
+  Write-ChocolateyFailure "$pkgName" "$($_.Exception.Message)"
+  throw
+}
---
+++ Devbox-Common/tools/chocolateyUninstall.ps1
@@ -0,0 +1,33 @@
+# Exact package name. Directory will be created
+# in choco\lib based on this value
+#
+$pkgName = 'Devbox-Common'
+
+try
+{
+  $varName = 'chocolatey_bin_root'
+
+  Update-SessionEnvironment
+
+  # Remove variable if not empty
+  #
+  $varValue = Get-Item Env:$varName -ErrorAction SilentlyContinue | Select -ExpandProperty Value -First 1
+
+  if ($varValue)
+  {
+    [Environment]::SetEnvironmentVariable($varName, $null, 'User')
+    Remove-Item Env:\$varName
+
+    Write-Host "Removed environment variable: $varName, value was: $varValue"
+  }
+
+  Write-ChocolateySuccess "$pkgName"
+}
+catch
+{
+  Write-ChocolateyFailure "$pkgName" "$($_.Exception.Message)"
+  throw
+}
+
+
+
