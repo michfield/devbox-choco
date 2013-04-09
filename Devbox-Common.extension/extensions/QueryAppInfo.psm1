7d2065c432ed628d509b41436bda5bf5c655a1a6
Michael Field <michfield@gmail.com>
2013-04-10 01:02:06 +0200

    Add helper function for install scripts

--- Devbox-Common.extension/extensions/QueryAppInfo.psm1
+++ Devbox-Common.extension/extensions/QueryAppInfo.psm1
@@ -108,3 +108,68 @@ function QueryAppInfo([string] $matchPattern = '', [bool] $firstOnly = $false)
 
     return $appArray
 }
+
+#
+# Throws a fatal error if a software with the same name pattern has been
+# found already installed. Used in install scripts, as a first check.
+# Returns nothing. Just breaks script execution, if needed.
+#
+# Usage:
+#
+#   FailOnAlreadyInstalledSoftware $packageName, "Oracle VM VirtualBox"
+#
+function FailOnAlreadyInstalledSoftware([string] $pkgName, [string] $pattern)
+{
+  # If set `-force` option on installation, query nothing - just
+  # continue with installation procedure
+  #
+  if ($force -eq $false) {
+    return
+  }
+
+  $arrMatch = QueryAppInfo($pattern)
+  $arrMatch = [array] $arrMatch
+
+  if ($arrMatch.Length -gt 0) {
+
+    $appInfo = $arrMatch[0]
+    $softwareName=$appInfo["DisplayName"]
+    $cmdUninstall=$appInfo["UninstallString"]
+
+    Write-Host ""
+    Write-Host "$softwareName version $($appInfo["DisplayVersion"]) is already installed." -foregroundcolor yellow
+
+    if ($arrMatch.Length -gt 1) {
+      Write-Host ""
+      Write-Host "CAUTION:" -foregroundcolor red
+      Write-Host "  I found multiple installed software with `'$pattern`' in its name." -foregroundcolor red
+      Write-Host "  So software is installed, only multiple times. And that can be a problem." -foregroundcolor red
+    }
+
+    Write-Host ""
+    Write-Host "Looks like `'$softwareName`' is installed separately by another Chocolatey package or not " -nonewline
+    Write-Host "using Chocolatey at all. Please uninstall `'$softwareName`' by removing that " -nonewline
+    Write-Host "other package or more often - you must uninstall `'$softwareName`' using standard " -nonewline
+    Write-Host "Windows uninstaller, the familiar `'Add/Remove Programs`' procedure."
+    Write-Host ""
+    Write-Host "If you decided to use standard Windows uninstall procedure, I'm trying to help you by " -nonewline
+    Write-Host "detecting a valid removal command. Here it is for you to try:"
+    Write-Host ""
+    Write-Host "$cmdUninstall" -foregroundcolor yellow
+    Write-Host ""
+    Write-Host "If you think that some other Chocolatey package installed `'$softwareName`', you can try " -nonewline -foregroundcolor darkgray
+    Write-Host "to analyze installed Chocolatey packages. However, be aware that most of current Chocolatey " -nonewline -foregroundcolor darkgray
+    Write-Host "packages don't provide you with a way to do uninstall." -foregroundcolor darkgray
+    Write-Host ""
+    Write-Host "» Find package containing `'$pattern`' in a name:" -foregroundcolor darkgray
+    Write-Host "  clist $pattern"
+    Write-Host "» Check if found Chocolatey package is currently installed:" -foregroundcolor darkgray
+    Write-Host "  cver pkgname -lo"
+    Write-Host "» If package is installed, uninstall it by typing:" -foregroundcolor darkgray
+    Write-Host "  cuninst pkgname"
+    Write-Host ""
+
+    Write-ChocolateyFailure "$pkgName" "Already installed"
+    throw
+  }
+}
