36ac47c1792258e3d80aef3cddeb73ad01081dfe
Michael Field <michfield@gmail.com>
2013-04-15 10:05:41 +0200

    VirtualBox package improvements in Uninstall procedure

--- Devbox-VirtualBox/tools/chocolateyInstall.ps1
+++ Devbox-VirtualBox/tools/chocolateyInstall.ps1
@@ -1,44 +1,50 @@
-﻿$packageName = 'Devbox-VirtualBox' # filename-safe value
-$installerType = 'exe'
-$url = 'http://download.virtualbox.org/virtualbox/4.2.10/VirtualBox-4.2.10-84105-Win.exe'
+﻿$packageName = "Devbox-VirtualBox" # filename-safe value
+$installerType = "exe"
+$url = "http://download.virtualbox.org/virtualbox/4.2.10/VirtualBox-4.2.10-84105-Win.exe"
 $url64 = $url
-$silentArgs = '-s'
+$silentArgs = "-silent"
 $validExitCodes = @(0)
 
-# Stop if software is already installed
-FailOnAlreadyInstalledSoftware($packageName, "Oracle VM VirtualBox")
+# Debug recommended:
+Set-StrictMode –version Latest
 
-$absToolsDir="$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+# Exit if software is already installed, except if forced
+# by `cinst -force` command line option
+#
+Stop-OnAppIsInstalled -pkg $packageName
+        -match "Oracle VM VirtualBox" `
+        -ignore "Oracle VM VirtualBox Guest Additions" `
+        -force $force
 
 try {
 
-  # We must import the certificates before installing package itself to
-  # avoid interactive prompt. Source: http://wpkg.org/Sun_xVM_VirtualBox
-  #
-  Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$absToolsDir\oracle.cer'" -validExitCodes $validExitCodes
+    # We must import the certificates before installing package itself to
+    # avoid interactive prompt. Source: http://wpkg.org/Sun_xVM_VirtualBox
+    #
+    $absToolsDir="$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+    Start-ChocolateyProcessAsAdmin "certutil -addstore 'TrustedPublisher' '$absToolsDir\oracle.cer'" -validExitCodes $validExitCodes
 
-  # Install package
-  #
-  Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+    # Install package
+    #
+    Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
 
-  # VirtualBox will set VBOX_INSTALL_PATH environment variable.
-  # Sometimes, it can contain multiple paths, as a leftover of old
-  # installations and updates. These multiple paths are properly
-  # seperated with semicolon, so it's safe simply all to append to a
-  # path.
-  #
-  # Source: https://github.com/mitchellh/vagrant/issues/885
-  #
-  Update-SessionEnvironment
-  $varValue = Get-Item Env:VBOX_INSTALL_PATH -ErrorAction SilentlyContinue | Select -ExpandProperty Value -First 1
-  Write-Host "Environment variable is set: VBOX_INSTALL_PATH = $varValue"
+    # VirtualBox will set VBOX_INSTALL_PATH environment variable. Sometimes,
+    # it can contain multiple paths, as a leftover of old installations
+    # and updates. These multiple paths are properly seperated with
+    # semicolon, so it's safe simply all to append to path.
+    #
+    # Source: https://github.com/mitchellh/vagrant/issues/885
+    #
+    Update-SessionEnvironment
+    $varValue = $Env:VBOX_INSTALL_PATH
+    Write-Host "Environment variable is set: VBOX_INSTALL_PATH = $varValue"
 
-  Install-ChocolateyPath $varValue
-  Write-Host "And also appended to the users PATH variable."
+    Install-ChocolateyPath $varValue
+    Write-Host "And also appended to the users PATH variable."
 
-  # Success
-  Write-ChocolateySuccess "$packageName"
+    # Success
+    Write-ChocolateySuccess "$packageName"
 } catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw
+    Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+    throw
 }
--- Devbox-VirtualBox/tools/chocolateyUninstall.ps1
+++ Devbox-VirtualBox/tools/chocolateyUninstall.ps1
@@ -1,35 +1,66 @@
-$pkgName = "Devbox-VirtualBox"
-$appPattern = "Oracle VM VirtualBox"
+# Reasoning explained:
+#
+#   If I have this Choco package, I will assume that VirtualBox has been
+#   installed with it. I can't just check for GUID because there is a
+#   possibility that user updated VirtualBox manually. I could also
+#   check version of installed application, and remove it only if
+#   it's after this one - but I won't do version check.
+#
+# Here is the logic:
+#   If we have this package, let's assume we installed VirtualBox with
+#   it, so, regardless of a version, remove any VirtualBox that you
+#   find on this machine.
 
-# GUID's
-#   v4.2.10 / {E28F112D-4784-4466-AE4B-07B3630C857F}
+$packageName = "Devbox-VirtualBox"
 
-# Strange: full application name (Oracle VM VirtualBox 4.2.10) contains
-# both the name and version. Maybe app is updated through normal way,
-# by update, I will just look for name without version.
+# Note: Strangely, full application name in registry contains
+# both the name and version (Oracle VM VirtualBox 4.2.10).
+#
+# GUID list:
+#   * v4.2.10 -> {E28F112D-4784-4466-AE4B-07B3630C857F}
+#
+#
+# Also, we can find ut a lot of information from this registry key:
+#     HKEY_LOCAL_MACHINE\SOFTWARE\Oracle\VirtualBox
+
+# Uninstall any version of VirtualBox
+# Note: must be outside catch block
+#
+$arrMatch = Show-AppUninstallInfo -match "Oracle VM VirtualBox" -ignore "Oracle VM VirtualBox Guest Additions"
 
 try
 {
-  # Uninstall any version of VirtualBox
-  #
-  $arrMatch = QueryAppInfo($appPattern)
-  $arrMatch = [array] $arrMatch
+    # Remember environment value, we will need it for cleanup
+    #
+    $varEnv = $Env:VBOX_INSTALL_PATH
 
-  if ($arrMatch.Length -gt 0) {
-    $appInfo = $arrMatch[0]
+    $arrMatch = [array] $arrMatch
+    if ($arrMatch.Length -gt 0) {
 
-    $softwareName = $appInfo["DisplayName"]
-    $cmdUninstall = $appInfo["UninstallString"]
-    $appGUID      = $appInfo["RegistryKeyName"]
+        $appInfo = $arrMatch[0]
+        $softwareName = $appInfo["DisplayName"]
+        $cmdUninstall = $appInfo["UninstallString"]
+        $appGUID      = $appInfo["RegistryKeyName"]
 
-    Uninstall-ChocolateyPackage "$pkgName" "msi" "$appGUID" -validExitCodes @(0)
-  }
+        Write-Host "`nUninstalling $softwareName version $($appInfo["DisplayVersion"])...`n" -foregroundcolor yellow
 
-  Write-ChocolateySuccess "$pkgName"
+        # Silent uninstall:
+        # msiexec.exe /qn /norestart /x{E28F112D-4784-4466-AE4B-07B3630C857F}
+        #
+        Uninstall-ChocolateyPackage "$packageName" "msi" "$appGUID" -validExitCodes @(0)
+    }
+
+    # Remove environment VBOX_INSTALL_PATH variable,
+    # and remove it's value from PATH
+    #
+    Remove-EnvironmentVariable "VBOX_INSTALL_PATH"
+    Remove-Path $varEnv
+
+    Write-ChocolateySuccess "$packageName"
 }
 catch
 {
-  Write-ChocolateyFailure "$pkgName" "$($_.Exception.Message)"
-  throw
+    Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+    throw
 }
 
