4eb26e04d74c1ddc25fc4388355bbc48c1f8a849
Michael Field <michfield@gmail.com>
2013-04-16 07:13:53 +0200

    Initial Vagrant package

---
+++ Devbox-Vagrant/tools/chocolateyInstall.ps1
@@ -0,0 +1,28 @@
+﻿$packageName = "Devbox-Vagrant"
+$installerType = "msi"
+$url = "http://files.vagrantup.com/packages/64e360814c3ad960d810456add977fd4c7d47ce6/Vagrant.msi"
+$url64 = $url
+$validExitCodes = @(0)
+
+Update-SessionEnvironment
+
+# Exit if software is already installed, except if forced
+#
+Stop-OnAppIsInstalled -pkg $packageName -match "Vagrant" -force $force
+
+try {
+
+    $installPath = join-path $(Get-RootPath) "Vagrant"
+    $silentArgs = "/passive INSTALLDIR=`"$installPath`""
+
+    # Install package.
+    # Vagrant should add itself to the PATH
+    #
+    Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+
+    # Success
+    Write-ChocolateySuccess "$packageName"
+} catch {
+    Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+    throw
+}
---
+++ Devbox-Vagrant/tools/chocolateyUninstall.ps1
@@ -0,0 +1,37 @@
+$packageName = "Devbox-Vagrant"
+
+# GUID list:
+#   * v1.1.5 -> {unknown}
+#
+
+Update-SessionEnvironment
+
+# Uninstall any version of Vagrant
+#
+$arrMatch = Show-AppUninstallInfo -match "Vagrant"
+
+try
+{
+    $arrMatch = [array] $arrMatch
+    if ($arrMatch.Length -gt 0) {
+
+        $appInfo = $arrMatch[0]
+        $softwareName = $appInfo["DisplayName"]
+        $cmdUninstall = $appInfo["UninstallString"]
+        $appGUID      = $appInfo["RegistryKeyName"]
+
+        Write-Host "`nUninstalling $softwareName version $($appInfo["DisplayVersion"])...`n" -foregroundcolor yellow
+
+        # Silent uninstall
+        #
+        Uninstall-ChocolateyPackage "$packageName" "msi" "$appGUID" -validExitCodes @(0)
+    }
+
+    Write-ChocolateySuccess "$packageName"
+}
+catch
+{
+    Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+    throw
+}
+
