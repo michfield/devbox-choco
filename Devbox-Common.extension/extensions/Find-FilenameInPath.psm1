767a74b2bb03c7b05d3dfdf96f381e53c0d869d4
Michael Field <michfield@gmail.com>
2013-04-16 18:25:37 +0200

    Improve ..file-in-path... functions

--- Devbox-Common.extension/extensions/Find-FilenameInPath.psm1
+++ Devbox-Common.extension/extensions/Find-FilenameInPath.psm1
@@ -2,11 +2,25 @@
 # Checks if filename is somewhere in a PATH environment variable paths
 #
 # Usage:
-#   Find-FileInPath "vbox*.*"
+#   Find-FileInEnvPath "vbox*.*"
 #
 # Author:
 #   Colovic Vladan, cvladan@gmail.com
 #
+
+# multiPath is a list of paths, joined with semicolon
+#
+function Find-FileInPath {
+param(
+    [string] $fileName,
+    [string] $multiPath
+)
+    $multiPath.Split(';') | ?{!([System.String]::IsNullOrEmpty($_))} | %{
+        if (Test-Path $_) {
+            ls $_ | ?{ $_.Name -like $fileName } | Select-Object -ExpandProperty Fullname -first 1
+        }
+    }
+}
 
 # Searches for paths that are part of PATH variable, and returns full
 # file-path of a found file specified by wildcard or empty if none has
@@ -14,25 +28,20 @@
 #
 # Returns an object, with fields: .FullName, .Name, .Directory
 #
-function Find-FilenameInPath {
+function Find-FileInEnvPath {
 param(
     [string] $fileName
 )
-    Write-Debug "Running 'Find-FileInPath' with wild-card filename:`'$fileName`'"
-
-    $env:PATH.Split(';') | ?{!([System.String]::IsNullOrEmpty($_))} | %{
-        if(Test-Path $_) {
-            ls $_ | ?{ $_.Name -like $fileName } | Select Fullname
-        }
-    }
+    Write-Debug "Running 'Find-FilenameInEnvPath' with wild-card filename:`'$fileName`'"
+    Find-FileInPath "$fileName" "$env:PATH"
 }
 
-function Test-FilenameInPath {
+function Test-FileInEnvPath {
 param(
     [string] $fileName
 )
     $isFound = $false
-    Find-FilenameInPath $fileName | %{$isFound = $true}
+    Find-FilenameInPath $fileName | %{ $isFound = $true }
     return $isFound
 }
 
