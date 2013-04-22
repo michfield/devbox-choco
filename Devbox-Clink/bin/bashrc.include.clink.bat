4bc455fc837ae766647483315af06d09f48dbf98
Michael Field <michfield@gmail.com>
2013-04-22 16:44:18 +0200

    Fix: not copying bin directory

--- Devbox-Clink/bin/bashrc.include.clink.bat
+++ Devbox-Clink/bin/bashrc.include.clink.bat
@@ -4,4 +4,4 @@
 :: This history alias is assuming you are using
 :: clink with default clink profile location
 ::
-doskey history=cat "%LOCALAPPDATA%\clink\.history" $*
+doskey history=type "%LOCALAPPDATA%\clink\.history" $*
