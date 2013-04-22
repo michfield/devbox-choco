58d5f8ac99e9d91fb8f757b960dc679345f9174a
Michael Field <michfield@gmail.com>
2013-04-22 23:13:31 +0200

    Fix setlocal problem in setenv.bat

--- Devbox-Common/bin/setenv.bat
+++ Devbox-Common/bin/setenv.bat
@@ -8,29 +8,23 @@
 :: With this batch file, there should be no need to reload command
 :: environment every time you want environment changes to propagate
 ::
-:: Sources:
-::  http://www.dostips.com/DtCodeBatchFiles.php#Batch.FindAndReplace
-::
 
 :: A strange trick to print without newline
 echo | set /p dummy="Reading environment variables from registry. Please wait... "
-
-:: These settings are default already - just to be sure
-setlocal ENABLEEXTENSIONS DISABLEDELAYEDEXPANSION
 goto main
 
 :: Set one environment variable from registry key
 :SetFromReg
-    "%WinDir%\System32\Reg" QUERY "%~1" /v "%~2" > "%TEMP%\setenv.tmp"
-    for /f "usebackq skip=2 tokens=2,*" %%A IN ("%TEMP%\setenv.tmp") do (
-        call set %~3=%%B
+    "%WinDir%\System32\Reg" QUERY "%~1" /v "%~2" > "%TEMP%\_envset.tmp"
+    for /f "usebackq skip=2 tokens=2,*" %%A IN ("%TEMP%\_envset.tmp") do (
+        echo/set "%~3=%%B"
     )
     goto :EOF
 
 :: Get a list of environment variables from registry
 :GetRegEnv
-    "%WinDir%\System32\Reg" QUERY "%~1" > "%TEMP%\getenv.tmp"
-    for /f "usebackq skip=2" %%A IN ("%TEMP%\getenv.tmp") do (
+    "%WinDir%\System32\Reg" QUERY "%~1" > "%TEMP%\_envget.tmp"
+    for /f "usebackq skip=2" %%A IN ("%TEMP%\_envget.tmp") do (
         if /I not "%%~A"=="Path" (
             call :SetFromReg "%~1" "%%~A" "%%~A"
         )
@@ -38,18 +32,29 @@ goto main
     goto :EOF
 
 :main
-    call :GetRegEnv "HKLM\System\CurrentControlSet\Control\Session Manager\Environment"
-    call :GetRegEnv "HKCU\Environment"
+    echo/@echo off >"%TEMP%\_env.bat"
 
-    :: Delete tempoorary files that we made
-    del /f /q "%TEMP%\setenv.tmp" 2>nul
-    del /f /q "%TEMP%\getenv.tmp" 2>nul
+    :: Slowly generating final file
+    call :GetRegEnv "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" >> "%TEMP%\_env.bat"
+    call :GetRegEnv "HKCU\Environment">>"%TEMP%\_env.bat" >> "%TEMP%\_env.bat"
 
-    :: Special handling for Path - mix both User and System
-    call :SetFromReg "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" Path Path
-    setlocal
-        set _vartmp=
-        call :SetFromReg "HKCU\Environment" Path _vartmp
-    endlocal & if not "%Path%"=="" if not "%u%"=="" set Path=%Path%;%u%
+    :: Special handling for PATH - mix both User and System
+    call :SetFromReg "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" Path Path_HKLM >> "%TEMP%\_env.bat"
+    call :SetFromReg "HKCU\Environment" Path Path_HKCU >> "%TEMP%\_env.bat"
 
+    :: Caution: do not insert space-chars before >> redirection sign
+    echo/set "Path=%%Path_HKLM%%;%%Path_HKCU%%" >> "%TEMP%\_env.bat"
+
+    :: In final file: remove temp variables
+    echo/set "Path_HKLM=" >> "%TEMP%\_env.bat"
+    echo/set "Path_HKCU=" >> "%TEMP%\_env.bat"
+
+    :: Cleanup
+    del /f /q "%TEMP%\_envset.tmp" 2>nul
+    del /f /q "%TEMP%\_envget.tmp" 2>nul
+
+    :: Set these variables
+    call "%TEMP%\_env.bat"
+
+    :: Finish
     echo | set /p dummy="Done."
