847db9a6d9e843c7152c6b0cd26dfb85a7549763
Michael Field <michfield@gmail.com>
2013-04-25 11:16:15 +0200

    Vargant upgraded to 1.2.2

--- Devbox-Vagrant/tools/chocolateyInstall.ps1
+++ Devbox-Vagrant/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = "Devbox-Vagrant"
 $installerType = "msi"
-$url = "http://files.vagrantup.com/packages/a7853fe7b7f08dbedbc934eb9230d33be6bf746f/Vagrant_1.2.1.msi"
+$url = "http://files.vagrantup.com/packages/7e400d00a3c5a0fdf2809c8b5001a035415a607b/Vagrant_1.2.2.msi"
 $url64 = $url
 $validExitCodes = @(0)
 
